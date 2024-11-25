b46fc4b027a22f21935c0f8f0ccd52faef39e3fc
Otthon Leão <otthonleao@hotmail.com>
2024-11-25 01:24:01 -0400

    chore: Initial commit

---
+++ src/main/java/dev/otthon/loanscope/LoanScopeApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.loanscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/otthon/loanscope/LoanScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.loanscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
