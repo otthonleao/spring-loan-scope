dafc7cec0fa5787626d7b16b8cbb2a447831a430
Otthon Leão <otthonleao@hotmail.com>
2024-11-26 03:07:48 -0400

    chore: add domain Loan and Customer

---
+++ src/main/java/dev/otthon/loan/domain/Customer.java
@@ -0,0 +1,14 @@
+package dev.otthon.loan.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Customer {
+
+    private Integer age;
+    private String cpf;
+    private String name;
+    private Double income;
+    private String location;
+
+}
---
+++ src/main/java/dev/otthon/loan/domain/Loan.java
@@ -0,0 +1,10 @@
+package dev.otthon.loan.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Loan {
+
+    private Customer customer;
+
+}
